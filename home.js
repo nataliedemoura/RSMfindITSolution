ae4e3bb1db1370371cf63b18778b35dd8ea27d78
kitkitkitkitkitkitkit <168396539+kitkitkitkitkitkitkit@users.noreply.github.com>
2025-12-07 14:45:30 -0500

    Update home.js

--- home.js
+++ home.js
@@ -333,7 +333,14 @@ async function submitSchedule() {
             requestedAt: firebase.firestore.FieldValue.serverTimestamp()
         });
         
-        // Send confirmation email using EmailJS                              
+        // Send confirmation email using EmailJS
+        
+        function sendEmail() {
+            const templateParams = {
+                name: document.querySelector("#name).value,
+                email: document.querySelector("#email).value,
+                subject: document.querySelector("#subject).value,
+                message: document.querySelector("#message).value,          
         emailjs.send('service_wwhe20q', 'template_c432pia', {
             student_name: name,
             student_email: email,
@@ -385,6 +392,7 @@ window.onload = async function() {
     }
 
 };
+
 
 
 
